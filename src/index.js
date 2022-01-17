436ccfc0cab3ebf5a5e2be719fbe01be7ae0dd0d
Kyra Nankivell <kyranankivell@Kyras-MBP.lan>
2022-01-17 11:51:14 -0500

    Prepare for Heroku deployment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
+import express from 'express';
+
+const app = express();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,3 +13,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
