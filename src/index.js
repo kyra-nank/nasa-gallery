371bc9aabeb7be579eb77c394150ecbe481aca4d
Kyra Nankivell <kyranankivell@Kyras-MBP.lan>
2022-01-17 12:00:47 -0500

    Troubleshoot

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
-import express from 'express';
+// import express from 'express';
 
-const app = express();
+// const app = express();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,5 +13,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT);
